e09e4f7016959007461210ef95ef0aae542c2b5f
hwrdtm <howard.tam.95@gmail.com>
2021-03-04 13:37:52 -0800

    implement bug fix for paths
    sometimes path may be a CDN url

--- test/util.ts
+++ test/util.ts
@@ -10,3 +10,8 @@ test('returns the path', t => {
 	const result: string = createValidUri('https://github.com/', 'https://github.com/rocktimsaikia');
 	t.is(result, 'https://github.com/rocktimsaikia');
 });
+
+test('returns CDN paths', t => {
+	const result: string = createValidUri('https://medium.com/', 'https://cdn-static-1.medium.com/_/fp/icons/Medium-Avatar-500x500.svg');
+	t.is(result, 'https://cdn-static-1.medium.com/_/fp/icons/Medium-Avatar-500x500.svg');
+});
