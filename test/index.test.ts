b70609bb1c89266c26ebe45f6ebd1f6703d0bac3
Rocktim Saikia <srocktim61@gmail.com>
2021-05-04 09:16:12 +0530

    test: update tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,9 +3,8 @@ import metaFetcher from '../src';
 describe('Main', () => {
   it('it should contain the meta-data properties', async (done) => {
     const response = await metaFetcher('https://google.com');
-    expect(response).toHaveProperty('basic_metadata');
-    expect(response).toHaveProperty('opengraph_social');
-    expect(response).toHaveProperty('opengraph');
+    expect(response).toHaveProperty('metadata');
+    expect(response).toHaveProperty('socials');
     expect(response).toHaveProperty('favicons');
     done();
   });
